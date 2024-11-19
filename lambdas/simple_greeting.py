2f0fe6f1414688860cfcc5fb89c3aab785bafb3b
Alejo <alejito23001@gmail.com>
2024-11-19 01:32:04 -0500

    hotfix: fix typo error

--- lambdas/simple_greeting.py
+++ lambdas/simple_greeting.py
@@ -3,4 +3,4 @@ import json
 
 def lambda_handler(event, context):
     response = {"statusCode": 200, "body": {"message": "Hola, bienvenido al sistema"}}
-    return json.dump(response)
+    return json.dumps(response)
